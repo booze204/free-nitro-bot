0007eb43c82039354e9ac2f6c63b848c53c632fd
Glitch (harmonious-crystalline-cry) <none>
2021-05-06 17:49:13 +0000

    💅🏌 Updated with Glitch

---
+++ MUTLAKA OKU.md
@@ -0,0 +1,11 @@
+Projeyi Remixledikten Sonra Kimsenin Sunucuna Saldırmaması İçin Projeni Gizlemeyi Unutma !
+
+• Proje Nasıl Gizlenir ?
+
+Sol Tarafta Projenin Adına Bas Ve Aç.
+Karşına Projen Ve Glitch Hakkında Bazı Şeyler Bulunan Bir Panel Çıkacak.( New Project , Remix Project , Switch Project )
+Bu Panelde Bulunan "Make This Project Private" 'nin yanında kilit ve onun solunda bir buton var.
+O Butona 1 Kere Bas VE O Kilitin Sarı olmasını Sağla.
+İşlem Bu Kadar.
+
+İyi Eğlenceler.
---
+++ bot.js
@@ -0,0 +1,74 @@
+const Discord = require('discord.js');
+const client = new Discord.Client();
+
+client.on('ready', () => {
+  console.log(`Bot Aktif :  ${client.user.tag}!`);
+});
+
+client.on('message', msg => {
+  if (msg.content === 'ping') {
+    msg.reply('Pong!');
+  }
+});
+
+
+
+client.on('message', message => { 
+	
+	function rasteleSembol(uzunluk, semboller) {
+
+var maske = '';
+
+if (semboller.indexOf('a') > -1) maske += 'abcdefghijklmnopqrstuvwxyz';
+
+if (semboller.indexOf('A') > -1) maske += 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+
+if (semboller.indexOf('0') > -1) maske += '0123456789';
+
+
+var sonuc = '';
+
+ 
+
+for (var i = uzunluk; i > 0; --i) 
+
+{
+
+sonuc += maske[Math.floor(Math.random() * maske.length)];
+
+}
+
+return sonuc;
+
+}
+
+function spamla(){
+	message.channel.send("https:/"+"/discord.gift/" + rasteleSembol(16,'0aA'));
+}
+
+	if (message.channel.id = "839918630909378560"){
+		if(message.content.startsWith("dmr!nitro")){ //ELLEME ÇALIŞMAZ :D
+		try{
+		var veri = message.content.toString();
+		var veriSn = veri.substring(10)
+ 		var saniye = parseInt(veriSn);
+ 		var saniye1 = saniye * 1000;
+		if (saniye > 1){
+		setInterval(spamla, saniye1);
+		}
+		
+		else{
+			message.reply("Bir Hata Oluştu. Bu Arada En Az 2 Saniye Girmelisin");
+		}
+		}
+		catch(err){
+			message.channel.send("Teknik bir hata oluştu!");
+		}
+		}
+		}
+ 	
+
+
+});
+
+client.login("ODM4ODQ2NTY5Njc1NDg5MzIx.YJBCvw.UePmMSddFqTt-P-RsfAWYCpKNBA");
